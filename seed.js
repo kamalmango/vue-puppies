7e73ae9a6159f4ede0b75ab6210b8015add3a4fa
Fred Zirdung <fred@hackreactor.com>
2016-05-21 09:56:09 -0700

    (init)

---
+++ seed.js
@@ -0,0 +1,18 @@
+/**
+ *  This file should be a standalone script that seeds your database,
+ *  making testing interactions with your database much easier.
+ *
+ *  You should be able to run this file from Terminal with:
+ *
+ *    node ./seed.js
+ *  
+ *  And populate your database with all the data from `data.json`
+ */
+
+
+// Step 1: Drop old data
+// TODO
+
+// Step 2: Add data from `data.json`
+// TODO
+
