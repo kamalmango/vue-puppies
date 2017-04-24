8a7bb4402c9c912092cd3b6a72c108c44873806e
Kamal Mango <kamal.mango7@gmail.com>
2017-04-24 16:09:48 +0200

    add components

--- client/app.js
+++ client/app.js
@@ -1,21 +1,32 @@
-new Vue({
-  el: '#puppies',
-  data: {
-    puppy: {
-      name: '',
-      owner: '',
-      image: '',
-      description: ''
-    },
-    puppies: [],
-    styleImage: {
-      height: '300px',
-      width: '300px'
-    }
-  },
-  created: function() {
-    this.fetchPuppies();
-  },
+Vue.component('nav-bar', {
+  template: `
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <a class="navbar-brand">Vue Puppies</a>
+      </div>
+    </nav>
+  `
+})
+
+Vue.component('form-add', {
+  props: ['puppy'],
+  template: `
+    <div class="panel-body">
+      <div class="form-group">
+        <input class="form-control" placeholder="Puppy's Name" v-model="puppy.name">
+      </div>
+      <div class="form-group">
+        <input class="form-control" placeholder="Puppy's Owner" v-model="puppy.owner"></textarea>
+      </div>
+      <div class="form-group">
+        <input class="form-control" placeholder="Puppy's Image url" v-model="puppy.image"></textarea>
+      </div>
+      <div class="form-group">
+        <textarea class="form-control" placeholder="Description" v-model="puppy.description"></textarea>
+      </div>
+      <button class="btn btn-primary" v-on:click="addPuppy">Submit</button>
+    </div>
+  `,
   methods: {
     addPuppy: function() {
       var vm = this;
@@ -32,6 +43,53 @@ new Vue({
         console.log(error);
       })
     }, 
+  }
+})
+
+Vue.component('puppy-card', {
+  props: ['puppy'],
+  template: `
+    <div class="row" :style="{marginTop: '50px', textAlign:'center', boxShadow: '0 2px 10px 0 rgba(88, 88, 88, 0.3)'}">
+      <div class="col-md-4">
+        <h4>
+          {{ puppy.name }}
+        </h4>
+        <img :src="puppy.image" :style="styleImage">
+      </div>
+
+      <div class="col-md-8">
+        <h5>
+          {{ puppy.owner }}
+        </h5>
+        <p>{{ puppy.description }}</p>
+      </div>
+    </div>
+  `, 
+  data: function() {
+    return {
+      styleImage: {
+        height: '300px',
+        width: '300px'
+      }
+    }
+  }
+})
+
+new Vue({
+  el: '#lend',
+  data: {
+    puppy: {
+      name: '',
+      owner: '',
+      image: '',
+      description: ''
+    },
+    puppies: []
+  },
+  created: function() {
+    this.fetchPuppies();
+  },
+  methods: {
     fetchPuppies: function() {
       var vm = this;
       axios.get('/api/puppies')
@@ -47,3 +105,15 @@ new Vue({
     }
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
